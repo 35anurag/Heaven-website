e0cfcc29a35fcbedb1777a55335e93bf81e5c5b1
35anurag <anuragregmi35@gmail.com>
2024-07-05 15:03:23 +0545

    code updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import Logo from "../../assets/logo.png";
-
+import { CiMenuBurger } from "react-icons/ci";
+import { IoClose } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [toggleButton, setToggleButton] = useState(false);
+
+  const handleNav = () => {
+    setToggleButton(!toggleButton);
+  };
+
+  const handleClose = () => {
+    setToggleButton(false);
+  };
+
   return (
     <section className="container navbar_container">
       <div className="navbar">
@@ -20,7 +31,26 @@ const Navbar = () => {
         </div>
 
         <button className="nav_button">Book Now</button>
+
+        <div className="open_button">
+          <button className="toggle_button" onClick={handleNav}>
+            {toggleButton ? <IoClose /> : <CiMenuBurger />}
+          </button>
+        </div>
       </div>
+
+      {toggleButton && (
+        <div className="nav_right">
+          <div className="nav_link">
+            <Link className="link" onClick={handleClose} to="/">Home</Link>
+            <Link className="link" onClick={handleClose} to="/about">About</Link>
+            <Link className="link" onClick={handleClose} to="/course">Course</Link>
+            <Link className="link" onClick={handleClose} to="/contact">Contact</Link>
+            {/* <button className="nav_button" onClick={handleClose}>Book Now</button> */}
+          </div>
+          
+        </div>
+      )}
     </section>
   );
 };
